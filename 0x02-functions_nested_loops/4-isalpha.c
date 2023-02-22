ebaa920257cb13e8bd82588d6485dcbc6763ecfc
Furqan518 <furqanoyebanjo@gmail.com>
2023-02-22 12:49:58 -0800

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for char alphabet
- * @c: the char to be checked *
- * Return: 1 if char is letter upper or lower, 0 otherwise
+ * _isalpha - checks for alphabetic character
+ * @c: The character to be checked
+ * Return: 1 for alphabetic character or 0 for anything else
  */
+
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'));
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#nclude "main.h"
+
+/**
+ * _abs - computes the absolute value of an int
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
